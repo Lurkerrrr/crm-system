1c900b6692ad54f63dfabc9cd70fa60e1c7d3513
debugger <meganesh2005@gmail.com>
2026-05-28 02:19:41 +0200

    Add sanity test to verify test project setup

---
+++ tests/CRMSystem.Tests/SanityTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CRMSystem.Tests;
+
+public class SanityTests
+{
+    [Fact]
+    public void Math_Should_Still_Work()
+    {
+        // Arrange
+        var a = 2;
+        var b = 3;
+
+        // Act
+        var result = a + b;
+
+        // Assert
+        result.Should().Be(5);
+    }
+}
